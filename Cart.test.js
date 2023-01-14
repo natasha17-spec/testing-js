268c20e74c6a6f197312041dbd40d8fbbe7c0586
ngerasimovich <ngerasimovich@elinext.com>
2023-01-14 13:57:55 +0300

    installing Ject npm package

--- Cart.test.js
+++ Cart.test.js
@@ -1,9 +1,14 @@
 const Cart = require('./Card')
-const assert = require('assert')
 
-const cart = new Cart();
-cart.addToCard('cheesecake');
-cart.removeItemCart('cheesecake')
-assert.deepStrictEqual(cart.items,[])
+test('The addToCart function can add an item to the cart',()=>{
+    const cart = new Cart();
+    cart.addToCard('cheesecake');
+    expect(cart.items).toEqual(['cheesecake'])
+})
 
-
+test('The removeFromCart function can remove an item to the cart',()=>{
+    const cart = new Cart();
+    cart.addToCard('cheesecake');
+    cart.removeItemCart('cheesecake');
+    expect(cart.items).toEqual([])
+})
