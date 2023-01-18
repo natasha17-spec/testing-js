8265454f9c48fb0fca9fbdfb50bd649aa11f6877
natasha17-spec <17061990@inbox.ru>
2023-01-18 22:01:12 +0300

    add returned value test

--- Cart/Inventory/inventoryController.test.js
+++ Cart/Inventory/inventoryController.test.js
@@ -5,11 +5,15 @@ beforeEach(async() =>{
 })
 
 test('cancel operation for invalid quantities', () => {
-    expect.hasAssertions();
    try {
-       addToInventory('cheesecake','not a number');
+       addToInventory('cheesecake','not a number').not.toThrow();
    } catch (e) {
        expect(inventory.get('cheesecake')).toBe(0);
    }
    expect(Array.from(inventory.entries())).toHaveLength(1)
+});
+
+test('returned value', ()=> {
+    const result = addToInventory('cheesecake', 2);
+    expect(result).toBe(2)
 });
