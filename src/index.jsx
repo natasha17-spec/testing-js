9b511e7a25e74115ff910a0ab00dee255a77e3c0
natasha17-spec <17061990@inbox.ru>
2023-02-05 20:06:08 +0300

    add prettier

--- src/index.jsx
+++ src/index.jsx
@@ -1,12 +1,5 @@
 import ReactDOM from "react-dom";
 import React from "react";
-
-const App = () => {
-    return (
-        <div>
-            <h1>Inventory Contents</h1>
-        </div>
-    );
-};
+import { App } from "./App.jsx";
 
 ReactDOM.render(<App />, document.getElementById("app"));
