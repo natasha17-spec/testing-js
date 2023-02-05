40c82bff90a5d80a322fc475e0f0d4df08b6ce31
natasha17-spec <17061990@inbox.ru>
2023-02-05 19:36:45 +0300

    base template react app

--- dbConnection.js
+++
@@ -1,7 +0,0 @@
-const db = require("knex")(require("./knexfile").development);//подготвавливает пул соединений для отладочной базы данных
-const closeConnection = ()=> db.destroy(); //уничтожает пул соединений
-
-module.exports = {
-    db,
-    closeConnection
-}
